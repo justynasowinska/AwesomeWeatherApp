aa4804465faafbde6d8bd622e0367a7ebdd67fe5
Justyna Sowińska <just.sowinska@gmail.com>
2024-12-02 23:39:01 +0100

    chore: add and configure React Navigation library

--- android/app/src/main/java/com/awesomeweatherapp/MainActivity.kt
+++ android/app/src/main/java/com/awesomeweatherapp/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.awesomeweatherapp
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -19,4 +20,11 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * Required for React Navigation to work properly.
+  */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
