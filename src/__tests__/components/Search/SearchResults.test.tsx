6c8862a50ca4caae83d77a96a260e08af6d6f61a
Justyna Sowińska <just.sowinska@gmail.com>
2024-12-04 20:20:15 +0100

    test: add unit tests for SearchResults component and update mock data

---
+++ src/__tests__/components/Search/SearchResults.test.tsx
@@ -0,0 +1,68 @@
+import { render, screen } from '@testing-library/react-native';
+import React from 'react';
+
+import { searchCitiesMockResponse } from '__mocks__/openWeatherResponseMock';
+import SearchResults from 'components/Search/SearchResults';
+import { Status } from 'hooks/useSearchCities';
+
+describe('SearchResults', () => {
+  it('renders loading state', () => {
+    render(
+      <SearchResults
+        data={[]}
+        status={Status.FETCHING}
+        error={null}
+        onCitySelect={jest.fn()}
+      />,
+    );
+
+    expect(screen.getByText('Loading...')).toBeOnTheScreen();
+  });
+
+  it('renders error state', () => {
+    render(
+      <SearchResults
+        data={[]}
+        status={Status.ERROR}
+        error="Something went wrong"
+        onCitySelect={jest.fn()}
+      />,
+    );
+
+    expect(screen.getByText('Error: Something went wrong')).toBeOnTheScreen();
+  });
+
+  it('renders no results message', () => {
+    render(
+      <SearchResults
+        data={[]}
+        status={Status.SUCCESS}
+        error={null}
+        onCitySelect={jest.fn()}
+      />,
+    );
+
+    expect(
+      screen.getByText(
+        'No results yet. Keep typing or try something different.',
+      ),
+    ).toBeOnTheScreen();
+  });
+
+  it('renders list of results', () => {
+    render(
+      <SearchResults
+        data={searchCitiesMockResponse.list}
+        status={Status.SUCCESS}
+        error={null}
+        onCitySelect={jest.fn()}
+      />,
+    );
+
+    expect(screen.getByText('New York, US')).toBeOnTheScreen();
+    expect(screen.getByText('Los Angeles, US')).toBeOnTheScreen();
+    expect(screen.getByText('Chicago, US')).toBeOnTheScreen();
+    expect(screen.getByText('Houston, US')).toBeOnTheScreen();
+    expect(screen.getByText('London, UK')).toBeOnTheScreen();
+  });
+});
