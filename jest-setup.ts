3a3241975014bc2f5292993a3fd58e11a621a6eb
Justyna Sowińska <just.sowinska@gmail.com>
2024-12-04 21:13:06 +0100

    chore: configure Jest to use fake timers and adjust transformIgnorePatterns

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,1 +1,2 @@
 import '@testing-library/react-native/extend-expect';
+jest.useFakeTimers();
--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
     'react-native-config': '<rootDir>/__mocks__/react-native-config.ts',
   },
   setupFilesAfterEnv: ['./jest-setup.ts'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-paper|@react-navigation|react-native-vector-icons)/)',
+  ],
 };
