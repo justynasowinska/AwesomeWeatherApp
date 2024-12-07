23b32bb6f3269fab240421ccc3888e0d1a06ec80
Justyna Sowińska <just.sowinska@gmail.com>
2024-12-07 10:19:51 +0100

    refactor: remove unused mock for react-native-config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   preset: 'react-native',
-  moduleNameMapper: {
-    'react-native-config': '<rootDir>/__mocks__/react-native-config.ts',
-  },
   setupFilesAfterEnv: ['./jest-setup.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|react-native-paper|@react-navigation|react-native-vector-icons)/)',
--- src/__mocks__/react-native-config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  OPEN_WEATHER_API_KEY: 'open-weather-api-key',
-};
