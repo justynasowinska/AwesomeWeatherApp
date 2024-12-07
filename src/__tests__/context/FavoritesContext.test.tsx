809c8fd913889efd41909aa9406e4c03f50ba776
Justyna Sowińska <just.sowinska@gmail.com>
2024-12-07 17:24:21 +0100

    test: add tests for FavoritesContext

---
+++ src/__tests__/context/FavoritesContext.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react-native';
+import { FavoritesProvider, useFavoritesContext } from 'context/FavoritesContext';
+import useFavorites from 'hooks/useFavorites';
+import React from 'react';
+import { Text } from 'react-native';
+import { City } from 'types/openWeather';
+
+jest.mock('hooks/useFavorites');
+jest.mock('@react-native-async-storage/async-storage', () =>
+	require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
+describe('FavoritesProvider', () => {
+  it('provides data and functions from useFavorites', () => {
+    const initialFavorites: City[] = [{ id: 1, name: 'City Initial', sys: { country: 'PL' }, coord: { lon: 1, lat: 2 } }];
+    const mockCity: City = { id: 2, name: 'City', sys: { country: 'PL' }, coord: { lon: 1, lat: 2 } };
+    const mockAddToFavorites = jest.fn();
+    const mockRemoveFromFavorites = jest.fn();
+    const mockClearAllFavorites = jest.fn();
+    const mockClearError = jest.fn();
+
+    (useFavorites as jest.Mock).mockReturnValue({
+      favorites: initialFavorites,
+      error: null,
+      addToFavorites: mockAddToFavorites,
+      removeFromFavorites: mockRemoveFromFavorites,
+      clearAllFavorites: mockClearAllFavorites,
+      clearError: mockClearError,
+    });
+
+    const ComponentUsingContext = () => {
+      const { favorites, addToFavorites, removeFromFavorites, clearAllFavorites, clearError } = useFavoritesContext();
+
+      React.useEffect(() => {
+        addToFavorites(mockCity);
+        removeFromFavorites(1);
+        clearAllFavorites();
+        clearError();
+      }, [addToFavorites, removeFromFavorites, clearAllFavorites, clearError]);
+
+      return <Text testID="favorites">{favorites[0].name}</Text>;
+    };
+
+
+
+    render(
+      <FavoritesProvider>
+        <ComponentUsingContext />
+      </FavoritesProvider>
+    );
+
+    expect(screen.getByTestId('favorites')).toHaveTextContent('City Initial');
+    expect(mockAddToFavorites).toHaveBeenCalledWith(mockCity);
+    expect(mockRemoveFromFavorites).toHaveBeenCalledWith(1);
+    expect(mockClearAllFavorites).toHaveBeenCalled();
+    expect(mockClearError).toHaveBeenCalled();
+  });
+});
