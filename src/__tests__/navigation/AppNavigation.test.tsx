2b83fcbb91b32d14771bd8e8b916b03730734bd4
Justyna Sowińska <just.sowinska@gmail.com>
2024-12-08 12:54:34 +0100

    test: add tests for AppNavigator

---
+++ src/__tests__/navigation/AppNavigation.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react-native';
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from 'navigation/AppNavigator';
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+	require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
+describe('AppNavigator', () => {
+  it('renders Home screen as the initial route', async () => {
+    render(
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    );
+
+    expect(await screen.findByPlaceholderText('Search for a city')).toBeOnTheScreen();
+  });
+});
