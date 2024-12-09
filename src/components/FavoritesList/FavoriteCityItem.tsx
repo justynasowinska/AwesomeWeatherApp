ef01ff3dbd3e81d905025f2120dbfe19256175be
Justyna Sowińska <just.sowinska@gmail.com>
2024-12-09 15:11:59 +0100

    feat: update FavoriteCityItem to use theme colors

--- src/components/FavoritesList/FavoriteCityItem.tsx
+++ src/components/FavoritesList/FavoriteCityItem.tsx
@@ -1,7 +1,7 @@
 import { FavoritesIcon } from 'components/common/FavoritesIcon';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { List } from 'react-native-paper';
+import { List, useTheme } from 'react-native-paper';
 import { City } from 'types/openWeather';
 
 interface FavoriteCityItemProps {
@@ -10,22 +10,26 @@ interface FavoriteCityItemProps {
   onPress: (city: City) => void;
 }
 
-const CityIcon = () => <List.Icon icon="city" />;
-
 const FavoriteCityItem = ({
   city,
   onRemove,
   onPress,
 }: FavoriteCityItemProps) => {
+  const { colors } = useTheme();
+
   const renderFavoritestIcon = () => (
     <FavoritesIcon isFavorite={true} onPress={() => onRemove(city.id)} />
+  );
+
+  const renderLeftIcon = () => (
+    <List.Icon icon="map-marker" color={colors.onSurfaceVariant} />
   );
 
   return (
     <List.Item
       title={`${city.name}, ${city.sys.country}`}
       description={`Lat: ${city.coord.lat}, Lon: ${city.coord.lon}`}
-      left={CityIcon}
+      left={renderLeftIcon}
       right={renderFavoritestIcon}
       style={styles.item}
       onPress={() => onPress(city)}
@@ -37,6 +41,7 @@ const styles = StyleSheet.create({
   item: {
     marginBottom: 8,
     paddingRight: 0,
+    paddingLeft: 8,
   },
 });
 
